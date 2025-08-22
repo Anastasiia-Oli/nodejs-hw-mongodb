d9a8bfc1d8fe9c5c082994db525b2e34bb906cc6
A_Oliinyk <aolejnik916@gmail.com>
2025-08-22 15:40:20 +0200

    change rout path for contact id

--- src/server.js
+++ src/server.js
@@ -35,7 +35,7 @@ export const setupServer = () => {
     });
   });
 
-  app.get('/contact/:contactId', async (req, res, next) => {
+  app.get('/contacts/:contactId', async (req, res, next) => {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
 
